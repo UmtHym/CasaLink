aab9e90cab602a2aaceabf727bfaf8449bfca29b
UmtHym <120595657+UmtHym@users.noreply.github.com>
2024-09-06 11:07:57 +0200

    firebase.config secured

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,17 +1,16 @@
-
 import { initializeApp } from "firebase/app";
-import{getFirestore} from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBRynDRBZ8UaTAWz-H3iPPcFRHI4fWUQrk",
-  authDomain: "house-marketplace-app-98213.firebaseapp.com",
-  projectId: "house-marketplace-app-98213",
-  storageBucket: "house-marketplace-app-98213.appspot.com",
-  messagingSenderId: "618177535645",
-  appId: "1:618177535645:web:794b7e2f509df388587e74"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
-export const db = getFirestore()
+export const db = getFirestore();
